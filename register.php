dee32f57f2aca87dfafea2a690d691a4606232d0
Peter Eze <pueze1@sheffield.ac.uk>
2016-05-13 11:52:38 +0100

    Indicated compulsory fields
    Indicated compulsory fields

--- register.php
+++ register.php
@@ -37,17 +37,17 @@ if(isset($_POST['register'])){
 																<option value="7">PGD</option>
 																<option value="8">M.Eng/M.Sc</option>
 																<option value="9">Ph.D</option>
-														</select></td>
+														</select><font color="red">*</font></td>
 					</tr>
 					<tr>
 						<td>Session Admitted</td><td><select name="sessionadmitted"><option value="0">--Select--</option>
 																<option value="2015_2016">2015/2016</option>
 																<option value="2016_2017">2016/2017</option>
 																
-														</select></td>
+														</select><font color="red">*</font></td>
 					</tr>
 					<tr>
-						<td>JAMB REG. No.</td><td><input type="text" name="jambno" placeholder="UTME Registration Number"></td>
+						<td>JAMB REG. No.</td><td><input type="text" name="jambno" placeholder="UTME Registration Number"><font color="red">*</font></td>
 					</tr>
 					<tr>
 								<td>Matriculation No.</td><td><input type="text" name="matricno" placeholder="University Number"></td>
@@ -60,12 +60,12 @@ if(isset($_POST['register'])){
 																<option value="SAAT">SAAT</option>
 																<option value="PGS">Postgraduate School</option>
 																
-														</select></td>
+														</select><font color="red">*</font></td>
 					</tr>
 					<tr>
 						<td>Department</td><td><select name="department" id="department"><option value="EEE">EEE</option>
 																
-														</select></td>
+														</select><font color="red">*</font></td>
 					</tr>
 					<tr>
 						<td>Specialisation</td><td><select name="specialisation" id="specialisation"><option value="none">None</option>
@@ -86,23 +86,23 @@ if(isset($_POST['register'])){
 
 					</tr>
 					<tr>
-						<td>Surname</td><td><input type="text" name="surname" placeholder="Family Name"></td>
+						<td>Surname</td><td><input type="text" name="surname" placeholder="Family Name"><font color="red">*</font></td>
 					</tr>
 					<tr>
-								<td>First Name</td><td><input type="text" name="firstname" placeholder="Given Name"></td>
+								<td>First Name</td><td><input type="text" name="firstname" placeholder="Given Name"><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>Middle Name</td><td><input type="text" name="middlename" placeholder="Other Name(s)"></td>
 							</tr>
 							<tr>
-								<td>Date of Birth</td><td><input type="text" name="dob" placeholder="DD-MM-YYYY"></td>
+								<td>Date of Birth</td><td><input type="text" name="dob" placeholder="DD-MM-YYYY"><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>Sex</td><td><select name="sex"><option value="0">--Select--</option>
 																<option value="M">Male</option>
 																<option value="F">Female</option>
 																<option value="O">Others</option>
-														</select></td>
+														</select><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>Marital Status</td><td><select name="maritalstatus"><option value="0">--Select--</option>
@@ -115,7 +115,7 @@ if(isset($_POST['register'])){
 														</select><font color="red">*</font></td>
 							</tr>
 							<tr>
-								<td>School Address</td><td><textarea name="schooladdress" rows="4"></textarea></td>
+								<td>School Address</td><td><textarea name="schooladdress" rows="4"></textarea><font color="red">*</font></td>
 							</tr>
 							
 				</table>
@@ -125,20 +125,20 @@ if(isset($_POST['register'])){
 
 
 							<tr>
-								<td>Home Address</td><td><textarea name="homeaddress" rows="6"></textarea></td>
+								<td>Home Address</td><td><textarea name="homeaddress" rows="6"></textarea><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>Country of Origin</td><td><select name="country"><option value="0">--Select--</option>
 																<option value="N">Nigeria</option>
 																<option value="O">Others</option>
-														</select></td>
+														</select><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>State of Origin</td><td><select name="state"><option value="0">--Select--</option>
 																<option value="abia">Abia</option>
 																<option value="adamawa">Adamawa</option>
 																<option value="foreigner">Foreigner</option>
-														</select></td>
+														</select><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>L.G.A of Origin</td><td><select name="lga"><option value="0">--Select--</option>
@@ -147,33 +147,33 @@ if(isset($_POST['register'])){
 														</select></td>
 							</tr>
 							<tr>
-								<td>Telephone</td><td><input type="text" name="telephone" placeholder="Mobile Number preffered"></td>
+								<td>Telephone</td><td><input type="text" name="telephone" placeholder="Mobile Number preffered"><font color="red">*</font></td>
 							</tr>
 							<tr>
-								<td>Email</td><td><input type="email" name="email" placeholder="Valid email address"></td>
+								<td>Email</td><td><input type="email" name="email" placeholder="Valid email address"><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>Mode of Study</td><td><select name="modeofstudy"><option value="0">--Select--</option>
 																<option value="fulltime">Full Time</option>
 																<option value="partime">Part Time</option>
 																
-														</select></td>
+														</select><font color="red">*</font></td>
 							</tr>
 							
 							<tr>
-								<td>Name of Parent/Guardian</td><td><input type="text" name="parentguardian" placeholder="Full Name"></td>
+								<td>Name of Parent/Guardian</td><td><input type="text" name="parentguardian" placeholder="Full Name"><font color="red">*</font></td>
 							</tr>
 							<tr>
-								<td>Next of Kin</td><td><input type="text" name="nextofkin" placeholder="Full Name"></td>
+								<td>Next of Kin</td><td><input type="text" name="nextofkin" placeholder="Full Name"><font color="red">*</font></td>
 							</tr>
 							<tr>
 								<td>Phone Number of Parent/Guardian</td><td><input type="text" name="phoneofparent" placeholder="Phone Number"></td>
 							</tr>
 							<tr>
-								<td>Phone Number of Next of Kin</td><td><input type="text" name="phoneofkin" placeholder="Phone Number"></td>
+								<td>Phone Number of Next of Kin</td><td><input type="text" name="phoneofkin" placeholder="Phone Number"><font color="red">*</font></td>
 							</tr>
 							<tr>
-								<td>Passport Upload</td><td><input type="file" name="passport" placeholder="Passport size Photograph"></td>
+								<td>Passport Upload</td><td><input type="file" name="passport" placeholder="Passport size Photograph"><font color="red">*</font></td>
 							</tr>
 							<tr><td><input type="submit" name="register" value="Register"></td><td><input type="Reset" value="Cancel"></td>
 							</tr>
@@ -184,6 +184,6 @@ if(isset($_POST['register'])){
 				</FORM>
 			</fieldset>
 		</div><!-- End of Register div -->
-		<div id="footer"> &copy Eze Peter U. (peter.eze@futo.edu.ng)</div>
+		<div id="footer"> &copy SEAS Team 2016. (contact: peter.eze@futo.edu.ng)</div>
 	</body>
 </html>
